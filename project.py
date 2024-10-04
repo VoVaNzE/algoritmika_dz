e851eec1790c7664f9f11ea93ea45acce34a97f5
VoVaNzE <102370022+VoVaNzE@users.noreply.github.com>
2024-10-04 17:31:52 +0300

    Update project.py

--- project.py
+++ project.py
@@ -2,19 +2,21 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.image import Image
+from kivy.uix.image import Image 
 
 class MyApp(App):
     def build(self):
         self.counter = 0
+        
         vl = BoxLayout(orientation='vertical')
         self.txt = Label(text="Счетчик: 0")
-        btn = Button(text="Потапай меня")
+        size_hint = 0.5,1
+        btn = Button(text="Потапай меня",background_normal='i.webp', background_down='i (1).webp', size_hint=size_hint, pos_hint={'x':0.25,'y':0.6})
         btn.bind(on_press=self.tapanie)
-        img = Image(source='i.webp')
+        # img = Image(source='i.webp')
         vl.add_widget(self.txt)
         vl.add_widget(btn)
-        vl.add_widget(img)
+        # vl.add_widget(img)
         return vl
 
     def tapanie(self, instance):
